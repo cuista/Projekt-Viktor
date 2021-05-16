67181604eacca478a9a177113c1d64700699bfe2
cuista <carlo.cristarellaorestano@gmail.com>
2021-05-16 21:44:24 +0200

    Added new enemy Turret Prefab with TurretAI script, added prefab Energyball and particle system bomb circle effect

--- Assets/Scripts/Controller/SceneControllerN.cs
+++ Assets/Scripts/Controller/SceneControllerN.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SceneControllerN : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private GameObject turretPrefab;
     private GameObject[] _enemies;
     public int enemiesCount;
 
@@ -25,11 +26,32 @@ public class SceneControllerN : MonoBehaviour
 
         for(int i=0; i<_enemies.Length; i++){
             if(_enemies[i]==null){
-                // INFO:Alternativa alla creazione di un enemy sotto nell'update:
-                _enemies[i]=Instantiate(enemyPrefab) as GameObject;
-                _enemies[i].transform.position=new Vector3(Random.Range(-70f,70f),1f,Random.Range(-70f,70f));
-                float angle = Random.Range(0,360f);
-                _enemies[i].transform.Rotate(0,angle,0);
+                if(i==_enemies.Length-1){
+                    _enemies[i]=Instantiate(turretPrefab,new Vector3(-9f,1f,9f),Quaternion.Euler(0,90f,0));
+                }
+                else if(i==_enemies.Length-2){
+                    _enemies[i]=Instantiate(turretPrefab,new Vector3(9f,1f,9f),Quaternion.Euler(0,-90f,0));
+                }
+                else if(i==_enemies.Length-3){
+                    _enemies[i]=Instantiate(turretPrefab,new Vector3(52f,1f,23f),Quaternion.Euler(0,-90f,0));
+                }
+                else if(i==_enemies.Length-4){
+                    _enemies[i]=Instantiate(turretPrefab,new Vector3(-23f,1f,48f),Quaternion.Euler(0,-180f,0));
+                }
+                else if(i==_enemies.Length-5){
+                    _enemies[i]=Instantiate(turretPrefab,new Vector3(48f,1f,-27f),Quaternion.Euler(0,0,0));
+                }
+                else if(i==_enemies.Length-6){
+                    _enemies[i]=Instantiate(turretPrefab,new Vector3(68f,1f,62.5f),Quaternion.Euler(0,-90f,0));
+                }
+                else
+                {
+                    // Alternative to gameObject creation with more lines
+                    _enemies[i]=Instantiate(enemyPrefab) as GameObject;
+                    _enemies[i].transform.position=new Vector3(Random.Range(-70f,70f),1f,Random.Range(-70f,70f));
+                    float angle = Random.Range(0,360f);
+                    _enemies[i].transform.Rotate(0,angle,0);
+                }
             }
         }
         
---
+++ Assets/Scripts/Enemies/TurretAI.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAI : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    public float rotationSpeed=3.0f;
+    public float range=30.0f;
+    public float fireDelay = 30f;
+
+    private Quaternion defaultRotation;
+    private float _shootTimer;
+    private bool _canShoot;
+
+    private bool _alive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultRotation = transform.rotation;
+        _shootTimer = 0;
+
+        _alive=true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position,range);
+        _canShoot = false;
+        foreach(var hitCollider in hitColliders)
+        {
+            GameObject hitOverlapSphere = hitCollider.transform.gameObject;
+            // check if player is within range
+            if(hitOverlapSphere.GetComponent<PlayerCharacter>() != null)
+            {
+                RaycastHit hitLinecast;
+                if (Physics.Linecast(transform.position, hitOverlapSphere.transform.position, out hitLinecast))
+                {
+                    // if there are NO obstacles between turret and player
+                    if(hitLinecast.transform.gameObject.GetComponent<PlayerCharacter>() != null)
+                    {
+                        Vector3 direction = (hitOverlapSphere.transform.position - transform.position).normalized;
+                        Quaternion toRotation = Quaternion.LookRotation(direction);
+                        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+                        transform.localEulerAngles = new Vector3(0,transform.localEulerAngles.y,0); // only y rotation
+                        _canShoot = true;
+                    }
+                }
+            }
+        }
+
+        if(_canShoot)
+        {
+            _shootTimer += Time.deltaTime;
+            if(_shootTimer > fireDelay)
+            {
+                GameObject bullet=Instantiate(bulletPrefab) as GameObject;
+                bullet.transform.position=transform.TransformPoint(Vector3.forward*2.5f);
+                bullet.transform.rotation=transform.rotation;
+                _shootTimer = 0;
+            }
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, defaultRotation, rotationSpeed * Time.deltaTime);
+            _shootTimer = 0;
+        }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        _alive=alive;
+    }
+}
